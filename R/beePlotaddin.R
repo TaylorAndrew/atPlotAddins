8a4d3ff52b2f5d342cbf82aa061e941cf0adc730
Andrew Taylor <taylor.andrew.r@gmail.com>
2016-07-12 11:31:06 -0400

    Make it 100%

--- R/beePlotaddin.R
+++ R/beePlotaddin.R
@@ -14,7 +14,7 @@ library(shiny)
     ),
     miniUI::miniTabPanel("Visualize", icon = shiny::icon("area-chart"),
      miniUI::miniContentPanel(
-         shiny::plotOutput("Plot", height = "80%")
+         shiny::plotOutput("Plot", height = "100%")
      )
    )
   )
